422a9761608fd860a7d18019add2523b2be518d3
Andres Barros <abarros17@cuc.edu.co>
2022-08-27 22:58:58 -0500

    agregar de la vista proyecto

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -6,12 +6,15 @@ import {
 } from "react-router-dom";
 import App from '../App';
 import Inicio from '../proyecto/view/Inicio';
-import { Proyecto } from '../proyecto/view/Proyecto';
+import Proyecto from '../proyecto/view/Proyecto';
+import { Proyectos } from '../proyecto/view/Proyectos';
+
 export const AppRouter = () => {
     return (
         <>
             <Routes>
                 <Route path="/" element={<Inicio />}/>
+                <Route path="/proyectos" element={<Proyectos />}/>
                 <Route path="/proyecto" element={<Proyecto />}/>
             </Routes>
         </>
