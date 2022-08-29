3945bf89db220c918ccc6562ef2068d600f768da
lortiz2022 <luis.ortiz@devitech.com.co>
2022-08-28 22:48:58 -0500

    nuev hookjs

---
+++ src/hooks/useHttpRequest.jsx
@@ -0,0 +1,10 @@
+import { useState } from "react"
+
+
+export const useHttpRequest = (url,webService,metodo, id='none' ) => {
+    
+    const [data, setdata] = useState({data:[]})
+  return {
+
+  }
+}
