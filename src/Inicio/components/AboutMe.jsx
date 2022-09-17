c5e8e0ac38b06d9c29fdc77ee6ddaa470939a6d7
lortiz2022 <107954011+lortiz2022@users.noreply.github.com>
2022-09-17 12:38:25 -0500

    cambios en el about me

--- src/Inicio/components/AboutMe.jsx
+++ src/Inicio/components/AboutMe.jsx
@@ -1,5 +1,5 @@
 import { Image } from '@mui/icons-material'
-import { Avatar, Card, CardHeader } from '@mui/material'
+import { Avatar, Box, Card, CardHeader, Grid } from '@mui/material'
 import React from 'react'
 
 export const AboutMe = ({ data }) => {
@@ -14,12 +14,18 @@ export const AboutMe = ({ data }) => {
                     <p className="text-gray-500">{data.puesto}</p>
                 </div>
             </div> */}
-            <Card sx={{ maxWidth: 345 }}>
-                <CardHeader
-                    avatar={
-                        <Image src={data.img} sx={{ width: 100, height: 100 }} />
-                    }
-                />
+            <Card sx={{ maxWidth: 345, padding: "30px", minHeight: "400px" }}>
+                <Grid container justifyContent="center" >
+                    <Grid item xs={12}>
+                        <img alt="testimonial" className="w-20 h-20 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100" src={data.img} />
+                    </Grid>
+                    <Grid item xs={12}>
+                        <p className="leading-relaxed">{data.descripcion}</p>
+                        <span className="inline-block h-1 w-10 rounded bg-indigo-500 mt-6 mb-4"></span>
+                        <h2 className="text-gray-900 font-medium title-font tracking-wider text-sm">{data.nombre}</h2>
+                        <p className="text-gray-500">{data.puesto}</p>
+                    </Grid>
+                </Grid>
             </Card>
 
         </>
--- src/Inicio/components/CarrouselItem.jsx
+++ src/Inicio/components/CarrouselItem.jsx
@@ -7,12 +7,15 @@ import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a lo
 
 export const CarrouselItem = ({ height, img }) => {
     return (
-        <div style={{maxHeight:"500px"}}>
+        <div style={{ maxHeight: "500px" }}>
             <Carousel showThumbs={false} showIndicators={false} showStatus={false} centerMode={false} useKeyboardArrows={true}>
-                <div style={{maxHeight:"500px"}}>
+                <div style={{ maxHeight: "500px" }}>
                     <img src={img.url} alt='sss' />
-                    <Box className=" bg-gray-900 bg-opacity-50" sx={{ width: "100%", height: "100%", position: "absolute", top: 0, right: 0 }}></Box>
-                    <p className="legend  bg-gray-700 bg-opacity-50 mb-5 " style={{fontSize:"16px"}}>En timecuc trabajamos para que los estudiantes de nuestra institución puedan graduarse oportunamente</p>
+                    <Box className=" bg-gray-200 bg-opacity-50" sx={{ width: "100%", height: "100%", position: "absolute", top: 0, right: 0 }}></Box>
+                    <Box padding={5} className=" bg-gray-900 bg-opacity-50" sx={{ width: "100%", height: "100%", position: "absolute", top: 0, right: 0 }}>
+                        <p className="legend  bg-gray-700 bg-opacity-50 mb-5 " style={{ fontSize: "16px" }}>En timecuc trabajamos para que los estudiantes de nuestra institución xspuedan graduarse oportunamente</p>
+                    </Box>
+
                 </div>
 
             </Carousel>
